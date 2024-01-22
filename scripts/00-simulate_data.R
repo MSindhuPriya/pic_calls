bb2cbe8ec2fd0ef345073582f4c3e30b8dc8a28f
MSindhuPriya <sindhupriya.mallavarapu@mail.utoronto.ca>
2024-01-22 14:04:55 -0500

    Add in R script for simulated data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -6,10 +6,30 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(janitor)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+# setting seed to ensure reproducibility
+set.seed(144)
+# creating a data frame
+simulated_pic_data <-
+  (tibble(
+    # creating a variable for the type of crisis event attended
+    Event = sample(
+      # list of the types of events
+      x = c("Suicide-related", "Overdose", "Other"), 
+      # sample 550 events from the list
+      size = 550,
+      # sample with replacement
+      replace = TRUE
+    ),
+    # creating a variable for the year the calls were attended
+    Year = 
+      # getting 50 samples for each year between 2014 and 2024
+      rep(c(2014:2024), 50)
+  ))
+# looking at the head of the simulated person in crisis data
+head(simulated_pic_data)
 
 
 
