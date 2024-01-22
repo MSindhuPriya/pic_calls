0e6c154168df1dcc0e9b565f8b46ba7db62df08a
MSindhuPriya <sindhupriya.mallavarapu@mail.utoronto.ca>
2024-01-22 12:45:38 -0500

    Adding in simulated data and intial set up of paper

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,15 @@
+#### Preamble ####
+# Purpose: Simulates data for attended person in crisis calls in Toronto between the year 2014 and 2024
+# Author: Sindhu Priya Mallavarapu
+# Date: 22 January 2024
+# Contact: sindhupriya.mallavarapu@mail.utoronto.ca
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Simulate data ####
+# [...ADD CODE HERE...]
+
+
+
